18eb9930262b96d973c512aea3c8f6f25b5ae307
Murr76 <51724253+Murr76@users.noreply.github.com>
2024-04-05 07:06:06 +0400

    Update Server.lua

--- Server.lua
+++ Server.lua
@@ -5,63 +5,66 @@ local Deleted = false
 local S_T = game:GetService("TeleportService")
 local S_H = game:GetService("HttpService")
 
-local function ReadFile()
-    local success, data = pcall(readfile, "server-hop-temp.json")
-    if success then
-        AllIDs = S_H:JSONDecode(data)
-    end
-end
-
-local function WriteFile(data)
+local File = pcall(function()
+    AllIDs = S_H:JSONDecode(readfile("server-hop-temp.json"))
+end)
+if not File then
+    table.insert(AllIDs, actualHour)
     pcall(function()
-        writefile("server-hop-temp.json", S_H:JSONEncode(data))
+        writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
     end)
 end
 
 local function TPReturner(placeId)
-    local Site;
+    local Site
     if foundAnything == "" then
-        Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100'))
+        Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Desc&limit=100'))
     else
-        Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+        Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Desc&limit=100&cursor=' .. foundAnything))
     end
-    local maxPlayersServer = nil
-    local maxPlayersCount = 0
+    local chosenServer
 
     if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
         foundAnything = Site.nextPageCursor
     end
 
-    for i, v in pairs(Site.data) do
-        local Possible = true
-        local ID = tostring(v.id)
-        local playerCount = tonumber(v.playing)
-        
-        if playerCount > maxPlayersCount and playerCount < tonumber(v.maxPlayers) then
-            maxPlayersServer = ID
-            maxPlayersCount = playerCount
+    for _, server in pairs(Site.data) do
+        if tonumber(server.maxPlayers) > tonumber(server.playing) then
+            chosenServer = server.id
+            break -- Stop loop after finding the first suitable server in descending order
         end
     end
 
-    if maxPlayersServer then
-        table.insert(AllIDs, maxPlayersServer)
-        wait()
-        WriteFile(AllIDs)
-        wait()
-        S_T:TeleportToPlaceInstance(placeId, maxPlayersServer, game.Players.LocalPlayer)
-        wait(4)
+    if chosenServer then
+        local Possible = true
+        for _, Existing in pairs(AllIDs) do
+            if chosenServer == tostring(Existing) then
+                Possible = false
+                break
+            end
+        end
+        if Possible then
+            table.insert(AllIDs, chosenServer)
+            wait()
+            pcall(function()
+                writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
+                wait()
+                S_T:TeleportToPlaceInstance(placeId, chosenServer, game.Players.LocalPlayer)
+            end)
+            wait(4)
+        end
     end
 end
 
 local module = {}
-function module:StartTeleport(placeId)
-    while true do
-        ReadFile()
-        TPReturner(placeId)
-        if foundAnything ~= "" then
+function module:Teleport(placeId)
+    while wait() do
+        pcall(function()
             TPReturner(placeId)
-        end
-        wait(120) -- 120 seconds = 2 minutes
+            if foundAnything ~= "" then
+                TPReturner(placeId)
+            end
+        end)
     end
 end
 
